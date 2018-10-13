4edbcead81e6105dd77935e4b5cac3451dcf1acf
Karimov R <rkarimov.r@gmail.com>
2018-10-14 00:58:02 +0400

    Add parallel stream example

---
+++ multithreading201/src/main/java/ru/csc/java2014/multithreading2/demo6/ParallelStreamBroken.java
@@ -0,0 +1,28 @@
+package ru.csc.java2014.multithreading2.demo6;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.DoubleAdder;
+
+public class ParallelStreamBroken {
+
+    public static void main(String[] args) {
+        int[] array = Commons.prepareArray();
+
+
+        long startTime = System.currentTimeMillis();
+
+        double[] sum = new double[1];
+//        DoubleAdder sum = new DoubleAdder();
+        Arrays.stream(array)
+                .parallel()
+                .mapToDouble(Commons::function)
+//                .forEach(sum::add);
+                .forEach(x -> sum[0] += x);
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("sum = " + sum[0]);
+//        System.out.println("sum = " + sum);
+        System.out.println("time = " + (endTime - startTime) + "ms");
+    }
+}
